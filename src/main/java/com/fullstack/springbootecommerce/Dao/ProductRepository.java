ffd28cdb8dc28dd7a0b98043c5c227d7b9e26114
Siddharth Kalidas <siddharthkalidas506@gmail.com>
2023-12-29 00:08:35 +0530

    enhance ProductListComponent to read category  id param

--- src/main/java/com/fullstack/springbootecommerce/Dao/ProductRepository.java
+++ src/main/java/com/fullstack/springbootecommerce/Dao/ProductRepository.java
@@ -3,9 +3,13 @@ package com.fullstack.springbootecommerce.Dao;
 
 import com.fullstack.springbootecommerce.entity.Product;
 import com.fullstack.springbootecommerce.entity.ProductCategory;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.data.domain.Pageable;
 
 /*
 HOSTNAME Syntax
@@ -18,4 +22,5 @@ http://locahost:4200 != http://locahost:8080
 @RepositoryRestResource(collectionResourceRel = "product", path = "products")
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository  extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
